72c0e24bbd95dc0e614e7314196ba694fb06b02b
Pierre Dok <pierredok@outlook.fr>
2025-09-08 18:47:10 +0200

    fixing opacity and marging burger menu

--- components/layout/navbar/index.tsx
+++ components/layout/navbar/index.tsx
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 import Grid from "@/components/ui/grid";
 import Link from "next/link";
 import { Menu, X, ChevronDown } from "lucide-react";
-
+import { motion, AnimatePresence } from "framer-motion";
 export default function LayoutNavbar() {
   const [openMenu, setOpenMenu] = useState(false);
   const [openIndex, setOpenIndex] = useState<number | null>(null);
@@ -87,12 +87,12 @@ export default function LayoutNavbar() {
     },
   ];
   return (
-    <nav>
-      <Grid className="gap-5">
+    <nav className="fixed inset-0 bg-white z-50 flex flex-col h-10 tablet:h-14 justify-center shadow-md">
+      <Grid>
         <div className="laptop:col-start-1 laptop:col-span-6 tablet:col-span-2 pl-5">
           <button
             type="button"
-            className="p-2 cursor-pointer"
+            className="p-2 cursor-pointer 0"
             onClick={() => {
               setOpenMenu(!openMenu);
             }}
@@ -110,36 +110,76 @@ export default function LayoutNavbar() {
             )}
           </button>
           {openMenu && (
-            <div className="absolute flex left-0 flex-col z-50 bg-gray-400 w-full">
-              <Link href="/">Accueil</Link>
-              {menuData.map((item, i) => (
-                <li key={i} className="group">
-                  <div
-                    onClick={() => setOpenIndex(openIndex === i ? null : i)}
-                    className="flex items-center justify-between w-full"
-                  >
-                    {item.label}
-                    <ChevronDown
-                      className={`${openIndex === i ? "rotate-180" : "rotate-0"} w-4 h-4 transition-transform duration-300`}
-                    />
-                  </div>
-                  {openIndex === i && (
-                    <ul className="ml-6 mt-2 space-y-1 text-sm text-gray-600">
-                      {item.sub.map((subItem, j) => (
-                        <li key={j}>
-                          <Link
-                            href={subItem.href}
-                            className="block hover:text-blue-600"
+            <AnimatePresence>
+              <>
+                <motion.div
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 0.2 }}
+                  exit={{ opacity: 0 }}
+                  transition={{ duration: 0.3 }}
+                  className="fixed top-14 inset-0 bg-black z-20"
+                  onClick={() => setOpenMenu(false)} // ferme si on clique dehors
+                />
+                <motion.div
+                  initial={{ x: "-100%", height: "100%" }}
+                  animate={{ x: 0, height: "100%" }}
+                  exit={{ x: "-100%", height: "100%" }}
+                  transition={{ duration: 0.3, ease: "easeInOut" }}
+                  className={`fixed top-10 tablet:top-14 bg-white z-50 flex flex-col p-6 w-full tablet:w-1/2 laptop:w-1/3`}
+                >
+                  <Link href="/">Accueil</Link>
+                  {menuData.map((item, i) => (
+                    <li key={i} className="group p-20">
+                      <div
+                        onClick={() => setOpenIndex(openIndex === i ? null : i)}
+                        className="flex items-center justify-between w-full p-5"
+                      >
+                        {item.label}
+                        <ChevronDown
+                          className={`${openIndex === i ? "rotate-180" : "rotate-0"} w-4 h-4 transition-transform duration-300`}
+                        />
+                      </div>
+                      <AnimatePresence>
+                        {openIndex === i && (
+                          <motion.ul
+                            initial={{
+                              height: 0,
+                              opacity: 0,
+                            }}
+                            animate={{
+                              height: "auto",
+                              opacity: 1,
+                            }}
+                            exit={{
+                              height: 0,
+                              opacity: 0,
+                            }}
+                            transition={{ duration: 0.3, ease: "easeInOut" }}
+                            className="pl-6 text-sm grid grid-cols-2 gap-3 overflow-hidden"
                           >
-                            {subItem.label}
-                          </Link>
-                        </li>
-                      ))}
-                    </ul>
-                  )}
-                </li>
-              ))}
-            </div>
+                            {item.sub.map((subItem, j) => (
+                              <motion.li
+                                key={j}
+                                initial={{ opacity: 0, y: -5 }}
+                                animate={{ opacity: 1, y: 0 }}
+                                transition={{ delay: j * 0.01 }}
+                              >
+                                <Link
+                                  href={subItem.href}
+                                  className="block hover:underline"
+                                >
+                                  {subItem.label}
+                                </Link>
+                              </motion.li>
+                            ))}
+                          </motion.ul>
+                        )}
+                      </AnimatePresence>
+                    </li>
+                  ))}
+                </motion.div>
+              </>
+            </AnimatePresence>
           )}
         </div>
 
@@ -150,7 +190,7 @@ export default function LayoutNavbar() {
           <div className="tablet:flex hidden">
             <Link href="/soutiens">Je soutiens</Link>
           </div>
-          <div>
+          <div className="">
             <Link href="/soutiens">S&#39;abonner</Link>
           </div>
         </div>
--- components/ui/grid/image/chevronDown.tsx
+++
@@ -1,7 +0,0 @@
-import { ChevronDown } from "lucide-react";
-
-const App = () => {
-  return <ChevronDown />;
-};
-
-export default App;
